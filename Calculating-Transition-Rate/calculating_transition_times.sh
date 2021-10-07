37590ca0166ace7025eabb57177d790a50677d72
CalvinNesbitt <cfn18@login-7.rcs.ic.ac.uk>
2021-10-07 09:57:19 +0100

    Made some changes to shell script

--- Calculating-Transition-Rate/calculating_transition_times.sh
+++ Calculating-Transition-Rate/calculating_transition_times.sh
@@ -1,4 +1,4 @@
-#PBS -l walltime=00:30:00
+#PBS -l walltime=24:00:00
 #PBS -l select=1:ncpus=8:mem=62gb
 #PBS -N Transition-Times
 #PBS -J 1-20
@@ -7,4 +7,4 @@ module load anaconda3/personal
 source activate personalpy3
 date
 
-python $PBS_O_WORKDIR/calculation_transition_times.py $PBS_ARRAY_INDEX
+python $PBS_O_WORKDIR/calculating_transition_times.py $PBS_ARRAY_INDEX
