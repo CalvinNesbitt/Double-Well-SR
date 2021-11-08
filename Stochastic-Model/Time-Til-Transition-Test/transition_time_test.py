f88a59936357a3dd83b074e16c97c20e76a0eada
CalvinNesbitt <cfn18@login-5.rcs.ic.ac.uk>
2021-11-08 17:29:33 +0000

    Tested timing test on cluster, seems to work

--- Stochastic-Model/Time-Til-Transition-Test/transition_time_test.py
+++ Stochastic-Model/Time-Til-Transition-Test/transition_time_test.py
@@ -25,14 +25,14 @@ import time as tm
 # Experiment Set Up
 
 # Total integration time = number_of_blocks * block_length
-number_of_blocks = int(1.e1) # Setting Experiment Length
+number_of_blocks = int(1.e7) # Setting Experiment Length
 block_length = 100. # How often we check for a transitions
 dt = 0.1
 time = np.arange(0, block_length, dt)
 
 # Experiment Parameters
-alpha = 0
-sigmas = [0.3, 0.25, 0.2, 0.18, 0.16, 0.14, 0.12, 0.1, 0.08, 0.06]
+alpha = 0.1
+sigmas = [0.4, 0.3, 0.25, 0.2, 0.18, 0.16, 0.14, 0.12, 0.1, 0.08, 0.06]
 sigma = sigmas[int(sys.argv[1]) - 1]
 p = [alpha, sigma]
 ic = cold_point 
@@ -61,7 +61,7 @@ def save_results():
     save_dir = save_directory(alpha)
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-    with open(save_dir + f'/sigma_{sigma:.3f}_results.pickle'.replace('.', '_'), 'wb') as handle:
+    with open(save_dir + f'/sigma_{sigma:.3f}_results'.replace('.', '_') + '.pickle', 'wb') as handle:
         pickle.dump(experiment_results, handle, protocol=pickle.HIGHEST_PROTOCOL)
     print(f'\nSaved Results at {save_dir}/sigma_{sigma:.3f}_results.pickle\n')
 
