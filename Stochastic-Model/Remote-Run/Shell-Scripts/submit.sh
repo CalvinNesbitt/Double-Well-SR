cda13c3d19d58158e2c5a9727c1249cf350397bc
Calvin Nesbitt <c.f.nesbitt@student.reading.ac.uk>
2021-11-11 12:54:01 +0000

    Attempted to fix error in shell script

--- Stochastic-Model/Remote-Run/Shell-Scripts/submit.sh
+++ Stochastic-Model/Remote-Run/Shell-Scripts/submit.sh
@@ -2,7 +2,7 @@
 NOW=$(date +"%Y-%m-%d-%T")
 run_directory="$EPHEMERAL/Stochastic-Well/$NOW"
 mkdir -p $run_directory
-cp -r $HOME/Instantons/Rotated-2D-Well/Stochastic-Model/Remote-Run/ $run_directory
+cp -r $HOME/Double-Well-SR/Stochastic-Model/Remote-Run/ $run_directory
 cd $run_directory/Remote-Run
 cp $run_directory/Remote-Run/Shell-Scripts/stochastic-well.sh .
 qsub stochastic-well.sh
