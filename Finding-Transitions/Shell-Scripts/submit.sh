57add5850057f4e54254158638ac283a91608657
CalvinNesbitt <cfn18@login-5.rcs.ic.ac.uk>
2021-11-16 12:03:37 +0000

    Edited transition finding script for new integration runs

--- Finding-Transitions/Shell-Scripts/submit.sh
+++ Finding-Transitions/Shell-Scripts/submit.sh
@@ -1,6 +1,6 @@
 # Make directory to copy model in to, submit job from there
 NOW=$(date +"%Y-%m-%d-%T")
-run_directory="$EPHEMERAL/Double-Well-Transitions/$NOW"
+run_directory="$EPHEMERAL/Double-Well-Finding-Transitions/$NOW"
 mkdir -p $run_directory
 cp -r $HOME/Double-Well-SR/Finding-Transitions/ $run_directory
 cd $run_directory/Finding-Transitions/
